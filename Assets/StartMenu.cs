d2cae87a4f32d7598f8494832bf8d9773521906b
Vauhgn Shorter <vwshorv@gmail.com>
2019-09-16 16:47:36 -0400

    Added all files

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+    public void SettingsPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PlayPress()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void STatsPress()
+    {
+        SceneManager.LoadScene(3);
+    }
+}

b337b857b97702148ba6ec3cf51e5eeef2741426
Jacob <jakesterc30@gmail.com>
2019-09-25 00:10:09 -0400

    Added text field that allows user to enter text

---
+++ Assets/InputFieldScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldScript : MonoBehaviour{
+	public InputField inputField;
+	
+	public string newText;
+	
+	public void Update(){
+		
+		//set newtext
+		newText = inputField.text;
+		
+		if(Input.GetKeyDown(KeyCode.Return)){
+		//display text in console on enter press
+		Debug.Log(newText);
+		
+		}
+	}
+	
+}
+		
+
+

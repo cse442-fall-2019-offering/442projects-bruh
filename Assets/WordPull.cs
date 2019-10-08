685ca0d13776b66053f8ddcf3a816a049d732084
Mark Kayutkin <markkayu@buffalo.edu>
2019-10-08 14:42:57 -0400

    added random word prompt to game screen

---
+++ Assets/WordPull.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordPull : MonoBehaviour
+{
+    public GameObject wordDisplay;
+
+    public void PullWord()
+    {
+        string[] allWords = System.IO.File.ReadAllLines(@"Assets\Words.txt");
+        int numWords = allWords.Length;
+        int index = Random.Range(0,numWords);
+        wordDisplay.GetComponent<Text>().text = allWords[index];
+    }
+
+}

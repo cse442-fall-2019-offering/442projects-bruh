2e090217d774ee69547c2fcfc6a6888d99fc7ca8
Willfarrell523 <willfarrell22@gmail.com>
2019-10-09 14:13:39 -0400

    Added distinct sliders for music+SFX volumes

--- Assets/setVol_script.cs
+++ Assets/setVol_script.cs
@@ -8,8 +8,19 @@ public class setVol_script : MonoBehaviour
 
     public AudioMixer mixer;
 
-    public void SetLevel (float sliderVal)
+    public void SetMasterLevel (float sliderVal)
     {
         mixer.SetFloat("MasterVolParam", Mathf.Log10(sliderVal) * 20);
     }
+
+    public void SetMusicLevel(float sliderVal)
+    {
+        mixer.SetFloat("MusicVolParam", Mathf.Log10(sliderVal) * 20);
+    }
+
+    public void SetSFXLevel(float sliderVal)
+    {
+        mixer.SetFloat("sfxVolParam", Mathf.Log10(sliderVal) * 20);
+    }
+
 }

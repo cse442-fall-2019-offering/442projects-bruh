d98298c502be75d3af51683b696987ce47c825f7
Willfarrell523 <willfarrell22@gmail.com>
2019-10-09 14:03:34 -0400

    Added volume sliders to settings menu

---
+++ Assets/setVol_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class setVol_script : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    public void SetLevel (float sliderVal)
+    {
+        mixer.SetFloat("MasterVolParam", Mathf.Log10(sliderVal) * 20);
+    }
+}

41a1a1859384fb964e2384f298eb8f356497112d
Mark <markkayu@buffalo.edu>
2019-11-13 17:03:42 -0500

    Sprint 3: Replaced music and logo to avoid copyright issues

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Threading;
+using UnityEngine.SceneManagement;
 /// <summary>
 ///  Controls pause functionality
 /// </summary>
 
 public class PauseScript : MonoBehaviour {
+	public GameObject timePanel;
+    public GameObject backgroundPanel;
+    public GameObject gameOverPanel;
+    public GameObject gameWonPanel;
+	public GameObject pausePanel;
+	public Scene gameScene;
 	public static bool isPaused = false;
 	
     // Pauses or resumes game based on if game is paused (isPaused)
@@ -15,10 +23,21 @@ public class PauseScript : MonoBehaviour {
 		if (isPaused) {				//Start game again
 			Time.timeScale = 1;
 			isPaused = false;
+			pausePanel.SetActive(false);
+			backgroundPanel.SetActive(true);
 		}
 		else {						//pause game
 			Time.timeScale = 0;
 			isPaused = true;
+			pausePanel.SetActive(true);
+			gameOverPanel.SetActive(false);
+			gameWonPanel.SetActive(false);
+			backgroundPanel.SetActive(false);
+			timePanel.SetActive(false);			//Disables the other panels/game elements other than the pause panel
 		}
+	}
+
+	public void restartGame(){
+		SceneManager.LoadScene(3);
 	}
 }
